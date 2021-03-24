780aa95fcce3673517104844f4c87445dd2c3e6b
kirbstomper <wignedkraby@gmail.com>
2021-03-23 22:01:50 -0400

    Fix some warning, add more error logging

--- main.go
+++ main.go
@@ -13,15 +13,6 @@ import (
 
 func main() {
 
-	http.HandleFunc("/generate", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodGet {
-			http.ServeFile(w, r, "plugin.jar")
-		} else {
-			w.WriteHeader(400)
-			fmt.Fprintf(w, "%q not allowed on this endpoint", r.Method)
-		}
-	})
-
 	http.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodPost {
 
@@ -37,19 +28,21 @@ func main() {
 			files := formdata.File["multiplefiles"] // grab the filenames
 			for i, _ := range files {
 				file, err := files[i].Open()
-				defer file.Close()
+
 				if err != nil {
 					fmt.Fprintln(w, err)
 					return
 				}
+				defer file.Close()
+
 				fmt.Println(files[i].Filename)
 				out, err := os.Create("Plugin\\icon_l.png") // Create the file
 
-				defer out.Close()
 				if err != nil {
-					fmt.Fprintf(w, "Unable to create the file for writing. Check your write access privilege", err)
+					fmt.Fprint(w, "Unable to create the file for writing. Check your write access privilege", err)
 					return
 				}
+				defer out.Close()
 
 				//Copy to the create file locally
 				_, err = io.Copy(out, file) // file not files [i]!
@@ -57,14 +50,17 @@ func main() {
 					fmt.Fprintln(w, err)
 					return
 				}
-				//TODO resize image to 32x32
 				leftimage, err := imaging.Open("Plugin\\icon_l.png")
-				//create flipped image and write to folder as icon_r
-				resizedleft := imaging.Resize(leftimage, 32, 32, imaging.Linear)
-				resizedright := imaging.FlipH(resizedleft)
+
+				if err != nil {
+					fmt.Fprintln(w, "Error opening image received, please enure file type is correct ", err)
+				}
+
+				resizedleft := imaging.Resize(leftimage, 32, 32, imaging.Linear) //Resize image
+				resizedright := imaging.FlipH(resizedleft)                       //Create flipped image
 				imaging.Save(resizedleft, "Plugin\\icon_l.png")
 				imaging.Save(resizedright, "Plugin\\icon_r.png")
-				//Write the files to a jar
+
 				writeToJar("plugin.jar")
 				http.ServeFile(w, r, "plugin.jar")
 			}
@@ -78,7 +74,6 @@ func main() {
 
 func writeToJar(filename string) {
 	// Create a buffer to write our archive to.
-
 	outFile, err := os.Create(filename)
 	if err != nil {
 		fmt.Println(err)
@@ -94,10 +89,14 @@ func writeToJar(filename string) {
 	files, _ := os.ReadDir("Plugin")
 	for _, file := range files {
 		f, err := w.Create(file.Name())
-
+		if err != nil {
+			fmt.Println(err)
+		}
 		//Read the file from the fileSystem
 		osFile, err := os.ReadFile("Plugin\\" + file.Name())
-
+		if err != nil {
+			fmt.Println(err)
+		}
 		_, err = f.Write(osFile)
 		if err != nil {
 			log.Fatal(err)
@@ -107,10 +106,14 @@ func writeToJar(filename string) {
 	files, _ = os.ReadDir("Plugin\\META-INF")
 	for _, file := range files {
 		f, err := w.Create("META-INF\\" + file.Name())
-
+		if err != nil {
+			fmt.Println(err)
+		}
 		//Read the file from the fileSystem
 		osFile, err := os.ReadFile("Plugin\\META-INF\\" + file.Name())
-
+		if err != nil {
+			fmt.Println(err)
+		}
 		_, err = f.Write(osFile)
 		if err != nil {
 			log.Fatal(err)
